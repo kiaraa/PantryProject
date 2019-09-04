e7321b39b3df960712d119f64f012c0fc47e8777
Kiara Atkins <atkinsk98@gmail.com>
2019-09-04 09:56:16 -0400

    Second initial commit

---
+++ src/main/java/com/kiaraatkins/PantryProj/PantryProjApplication.java
@@ -0,0 +1,13 @@
+package com.kiaraatkins.PantryProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantryProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PantryProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiaraatkins/PantryProj/PantryProjApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kiaraatkins.PantryProj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PantryProjApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
